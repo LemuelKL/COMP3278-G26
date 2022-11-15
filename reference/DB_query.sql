a51e29e149eb4f61bb3d37330d3aeb76b16a2e82
skywcs <skywcs@connect.hku.hk>
2022-11-16 01:42:35 +0800

    Modified DB_query

--- reference/DB_query.sql
+++ reference/DB_query.sql
@@ -7,8 +7,9 @@
 -- Server version: 5.7.28-0ubuntu0.18.04.4
 -- PHP Version: 7.2.24-0ubuntu0.18.04.1
 
+# Set to UTC+8
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET time_zone = "+08:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -21,15 +22,11 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `Student`
---
-DROP TABLE IF EXISTS `Student`;
+DROP TABLE IF EXISTS `Record`;
 DROP TABLE IF EXISTS `HasCourse`;
 DROP TABLE IF EXISTS `Course`;
-DROP TABLE IF EXISTS `Record`;
 DROP TABLE IF EXISTS `TimeSlot`;
+DROP TABLE IF EXISTS `Student`;
 
 # Create TABLE 'Student'
 CREATE TABLE Student (
@@ -64,13 +61,17 @@ INSERT INTO `TimeSlot`
     (7, '15:30', '16:25'),
     (8, '16:30', '17:25'),
     (9, '17:30', '18:25'),
-    (10, '18:30', '19:25');
+    (10, '18:30', '19:25'),
+    (11, '19:30', '20:25'),
+    (12, '20:30', '21:25'),
+    (13, '01:30', '02:25');
 
 # Create TABLE 'Course'
 CREATE TABLE `Course` (
   `course_id` int NOT NULL,
+  `course_title` varchar(30) NOT NULL,
   `timeSlot_id` int NOT NULL,
-  `day` int NOT NULL, # Range [1,5] 1: Mon, 2: Tuesday...
+  `day` int NOT NULL, # Range [0,4] 0: Mon, 1: Tue...
   `classroom` varchar(60) NOT NULL,
   `message` varchar(200),
   `zoomLink` varchar(200),
@@ -81,16 +82,16 @@ CREATE TABLE `Course` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO Course 
-    VALUES (1, 1, 1, 'Meng Wah Complex MWT2', 'Welcome to DBMS', 'https://hku.zoom.us/rec/play/JR7T4-Hk_AfD1sk1rm6qPzrnukGBMcj5wEDoiiEcje9g78dU8vDNtkAMIJYORJtOn5An54ls6lsX6Pdp.qjBu_xlFu3g_MZw4?continueMode=true', 'None', 'None'),
-    (2, 3, 1, 'Central Prodium, Centennial CPD-LG.01', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (3, 4, 1, 'Chong Yuet Ming Physics CYPP2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (4, 8, 1, 'Library Extension Building LE4', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (5, 1, 2, 'Central Prodium, Centennial CPD-LG.01', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (6, 4, 3, 'Chong Yuet Ming Physics CYPP2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (7, 6, 3, 'Meng Wah Complex MWT2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (8, 3, 4, 'Central Prodium, Centennial CPD-LG.01', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (9, 4, 4, 'Library Extension Building LE4', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
-    (10, 9, 5, 'Chong Yuet Ming Physics CYPP2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None');
+    VALUES (1, 'Intro to DBMS1', 1, 1, 'Meng Wah Complex MWT2', 'Welcome to DBMS', 'https://hku.zoom.us/rec/play/JR7T4-Hk_AfD1sk1rm6qPzrnukGBMcj5wEDoiiEcje9g78dU8vDNtkAMIJYORJtOn5An54ls6lsX6Pdp.qjBu_xlFu3g_MZw4?continueMode=true', 'None', 'None'),
+    (2, 'Intro to DBMS2', 3, 0, 'Central Prodium, Centennial CPD-LG.01', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (3, 'Intro to DBMS3', 4, 0, 'Chong Yuet Ming Physics CYPP2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (4, 'Intro to DBMS4', 8, 0, 'Library Extension Building LE4', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (5, 'Intro to DBMS5', 1, 1, 'Central Prodium, Centennial CPD-LG.01', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (6, 'Intro to DBMS6', 4, 2, 'Chong Yuet Ming Physics CYPP2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (7, 'Intro to DBMS7', 6, 3, 'Meng Wah Complex MWT2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (8, 'Intro to DBMS8', 3, 3, 'Central Prodium, Centennial CPD-LG.01', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (9, 'Intro to DBMS9', 4, 3, 'Library Extension Building LE4', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None'),
+    (10, 'Intro to DBMS10', 13, 2, 'Chong Yuet Ming Physics CYPP2', 'Welcome students', 'https://hku.zoom.us/rec/play/44RSxgjo8sEQbDbCnj5MGU12iphMNNEdfsQ0muIhDlg5K2EaXOUOcVYwfBcOHSHnPmdg8WIpr8cz_rrt._lU525ClQSSUWqMo?continueMode=true', 'None', 'None');
 
 
 # Create TABLE 'Record'
@@ -123,8 +124,9 @@ CREATE TABLE `HasCourse` (
 #Insert sample data into HasCourse
 INSERT INTO `HasCourse`
     VALUES (1,1),
-    (1,2),
+    (1,10),
     (1,3),
+    (1,2),
     (2,2),
     (2,3),
     (3,1),
@@ -132,13 +134,35 @@ INSERT INTO `HasCourse`
     (3,3);
 
 
-#LOCK TABLES `Student` WRITE;
-/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-#INSERT INTO `Student` VALUES (1, "JACK", NOW(), '2021-01-20');
-/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
-#UNLOCK TABLES;
 
+# Operations 
+# Get the student_id by username (Input: username)
+Select student_id from Student
+    where username = 'Sky';
 
+# Get the upcoming lecture (Input: student_id)
+Select * from Student as s, hasCourse as hc, Course as c, TimeSlot as t
+    where s.student_id = hc.student_id 
+    and c.course_id = hc.course_id
+    and t.timeSlot_id = c.timeSlot_id
+    and s.student_id = 1
+    and day = WEEKDAY(CURDATE())
+    and (CURRENT_TIME() between start_time and end_time OR TIMEDIFF('01:30', CURRENT_TIME()) < '01:00');
+
+# Get timetable (Input: student_id)
+Select course_title, classroom, start_time, end_time from Student as s, hasCourse as hc, Course as c, TimeSlot as t
+    where s.student_id = hc.student_id 
+    and c.course_id = hc.course_id
+    and t.timeSlot_id = c.timeSlot_id
+    and s.student_id = 1;
+
+# Insert Record (Input: student_id, login_time, logout_time)
+Insert into Record
+    values (Input);
+
+# Get Record (Input: student_id)
+Select * from Record
+    where student_id = 1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
